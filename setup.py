e6c7a00886d6ab753039cfef8d1d8b9b97ac3f11
Keith1815 <keith1815@live.com>
2023-11-05 18:25:42 +0000

    5-11-23 1825

--- setup.py
+++ setup.py
@@ -138,10 +138,10 @@ setup(
     # # If there are data files included in your packages that need to be
     # # installed, specify them here.
     # package_data={  # Optional
-    #     'sample': ['package_data.dat'],
-    #     "sample": ["package_data.dat"],
-    # },
-
+    #      'sample': ['package_data.dat'],
+    #      "sample": ["package_data.dat"],
+    #  },
+    include_package_data=True,
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
