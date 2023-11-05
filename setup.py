9c98f4f74a1473a23865cb58ff9cfee2df5f53ec
Keith1815 <keith1815@live.com>
2023-11-05 20:12:33 +0000

    5-11-23 2012

--- setup.py
+++ setup.py
@@ -137,10 +137,12 @@ setup(
 
     # # If there are data files included in your packages that need to be
     # # installed, specify them here.
-    package_data={  # Optional
-          'src': ['uob.png'],
-      },
+    # package_data={  # Optional
+    #       'src': ['uob.png'],
+    #   },
+    
     include_package_data=True,
+    
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
