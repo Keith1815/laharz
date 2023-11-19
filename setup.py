c6c46e17b81a862ca96295cdcbb5abc087732777
Keith1815 <keith1815@live.com>
2023-11-19 09:44:13 +0000

    19/11/23 0944

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ setup(
     install_requires=['shapely>=2.0.2', 
                       'rasterio>=1.3.9', 
                       'geopandas>=0.14.0',
-                      'numpy>=1.26.0' 
+                      'numpy>=1.26.0', 
                       'scipy>=1.11.3', 
                       'pillow>=10.1.0'],
     # Optional
